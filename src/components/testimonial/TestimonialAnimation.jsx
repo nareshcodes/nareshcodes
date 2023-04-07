63d38953880ff52d2a48ea804fc6e847c299d68a
NARESHCODES <nareshcodes@gmail.com>
2023-04-07 20:43:01 +0545

    changed image

--- src/components/testimonial/TestimonialAnimation.jsx
+++ src/components/testimonial/TestimonialAnimation.jsx
@@ -31,21 +31,21 @@ export default function SimpleSlider() {
 
   const TestimonilContent = [
     {
-      imageName: "team1",
+      imageName: "team-1",
       desc: `  I highly recommend for anyone in need of a professional and reliable web developer. They were easy to work with, communicative, and always delivered work on time. Their ability to turn my vision into a reality was truly impressive.`,
       reviewerName: "It Training Nepal",
       designation: "CEO @ Eprabidihi",
       delayAnimation: "",
     },
     {
-      imageName: "team2",
+      imageName: "team-2",
       desc: ` Working with Naresh was an absolute pleasure. They listened carefully to my needs and delivered a beautiful, user-friendly website that exceeded my expectations. Their attention to detail and technical expertise are unparalleled`,
       reviewerName: "Broadway Infosys",
       designation: "sales @ broadwayInfosys",
       delayAnimation: "200",
     },
     {
-      imageName: "team3",
+      imageName: "team-3",
       desc: `I highly recommend Naresh, it was an amazing experience. They took the time to understand my vision and built an app that was both visually stunning and incredibly functional. Their expertise in app development is second to none.`,
       reviewerName: "codesoft",
       designation: "CEO @ codesoft",
